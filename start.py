576625b8591acc741aa84ff51034b93fbd6c5d51
mercury <udtrokia@gmail.com>
2018-08-13 21:17:59 +0800

    [LOOP]

--- start.py
+++ start.py
@@ -11,15 +11,20 @@ def fetch_tx(block):
     block.get_contents();
     return block;
 
-def store_blocks():
-    db.FoxDB().init();    
-    db.FoxDB().start();
-    
+def mix_height():
     local_height = db.Block.select().count();
     remote_height = int(tx.Block().get_last()[0]);    
 
     if db.Block.get(db.Block.height == local_height).finished == 1:
         local_height += 1;
+
+    return (local_height, remote_height);
+
+def store_blocks():
+    db.FoxDB().init();    
+    db.FoxDB().start();
+    
+    (local_height, remote_height) = mix_height();
 
     for i in Bar('Stored Blocks:').iter(range(local_height, remote_height)):
         block = fetch_block(i);
@@ -31,12 +36,19 @@ def store_blocks():
             txs = block.txs,
             finished = 1,
         )
-        # print("Store Block", block.height, "...");
 
-store_blocks();
+
+def mian():
+    while True:
+        try:
+            store_blocks();
+        except:
+            continue;
+
+main();
+
 def test():
-    t = db.Block.select();
-    for i in t:
-        print(i.time)
+    t = db.Block.select().count();
+    print(t);
 
 # test();
